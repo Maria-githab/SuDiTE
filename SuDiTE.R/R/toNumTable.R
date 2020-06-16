b5e961b53c88b1667604bdaa305da84fdcac8c6a
Maria-githab <mariatemirkaeva@gmail.com>
2020-06-16 22:38:17 +0500

    add toNumT_direct

---
+++ SuDiTE.R/R/toNumTable.R
@@ -0,0 +1,20 @@
+#' Transformations of factors to logical variables
+#'
+#' @param X is the covariate matrix
+#'
+#' @return returns a numeric-only covariate matrix
+#' @export
+#'
+#' toNumericTable()
+#'
+toNumericTable=function(X) {
+  require(data.table)
+  d=data.table(X)
+  for( col in colnames(d) ) {
+    if( is.factor(d[[col]]) ) {
+      d[, c(paste0(col,".",levels(d[[col]]))[-1], col) :=
+          c(lapply(levels(d[[col]])[-1], function(x) as.integer(x == d[[col]])), .(NULL))]
+    }
+  }
+  return(as.data.frame(d))
+}
